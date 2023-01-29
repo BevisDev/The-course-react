859f175b470a3b5b7885336070691d69d27a93e6
BevisDev <trthanhbinh5720@gmail.com>
2023-01-29 21:16:23 +0700

    Upload source code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import React from "react";
+import SignUpFormHook from "./components/form/SignUpFormHook";
+function App() {
+  return (
+    <div>
+      <SignUpFormHook></SignUpFormHook>
+    </div>
+  );
+}
+
+export default App;
